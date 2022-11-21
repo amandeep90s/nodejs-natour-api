b70e8b1ccf53dc37eff665da3c213467b5484474
Amandeep Singh <programmeramandeep@gmail.com>
2022-11-21 15:37:24 +0530

    Handling api get requests

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hi folks', app: 'natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+  true
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    results: tours.length,
+    data: { tours: tours },
+  });
 });
 
 const port = 3000;
