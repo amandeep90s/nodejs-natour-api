41780915e257de3bd8e39ee5931f520ff4e265e0
Amandeep Singh <programmeramandeep@gmail.com>
2022-11-23 21:16:44 +0530

    Connecting mongodb database with mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 // Need to define before app initialization
 dotenv.config({ path: `${__dirname}/config.env` });
 
-const app = require('./app');
+// Connecting to db
+mongoose.connect(process.env.DATABASE_URI || '').catch((error) => {
+  throw error;
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
