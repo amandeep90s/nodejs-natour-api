a1fd93666e61aa224370f41162d5422886c5b0d8
Amandeep Singh <programmeramandeep@gmail.com>
2022-11-24 15:40:35 +0530

    Deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,8 +53,13 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: error });
+  }
 };
 
 module.exports = {
