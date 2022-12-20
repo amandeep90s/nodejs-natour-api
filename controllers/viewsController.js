69c0ab7dad0ce30f46859d9a90f046906a57e21f
Amandeep Singh <programmeramandeep@gmail.com>
2022-12-20 20:15:00 +0530

    Building the Tour Overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,10 @@
-const getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 const getTour = (req, res) => {
   res.status(200).render('tour', { title: 'The Forst Hiker Tour' });
