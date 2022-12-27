a18ee710858f930aa7fefa5c1fdfc4107b49af2a
Amandeep Singh <programmeramandeep@gmail.com>
2022-12-27 11:18:02 +0530

    Image Uploads Using Multer Users

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const {
   getAllUsers,
   getUser,
@@ -20,7 +21,9 @@ const {
   restrictTo,
 } = require('../controllers/authController');
 
+const upload = multer({ dest: 'public/img/users' });
 const router = express.Router();
+
 router.post('/signup', signUp);
 router.post('/login', login);
 router.get('/logout', logout);
@@ -30,7 +33,7 @@ router.patch('/reset-password/:token', resetPassword);
 router.use(protectedRoute);
 router.patch('/update-password', updatePassword);
 router.get('/me', getMe, getUser);
-router.patch('/update-me', updateMe);
+router.patch('/update-me', upload.single('photo'), updateMe);
 router.delete('/delete-me', deleteMe);
 // Restricted all routes after this to admin only
 router.use(restrictTo('admin'));
