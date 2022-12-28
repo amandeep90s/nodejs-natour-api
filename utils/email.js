7515bbfa8c78173a8fe6f6070f0eb24b18450185
Amandeep Singh <programmeramandeep@gmail.com>
2022-12-28 13:47:38 +0530

    Building a Complex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,22 +1,52 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.USER_EMAIL_HOST,
-    port: process.env.USER_EMAIL_PORT,
-    auth: {
-      user: process.env.USER_EMAIL_USERNAME,
-      pass: process.env.USER_EMAIL_PASSWORD,
-    },
-  });
-  const mailOptions = {
-    from: 'John Doe <johndoe@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Adminstrator <${process.env.EMAIL_FROM}>`;
+  }
 
-  await transporter.sendMail(mailOptions);
-};
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
 
-module.exports = sendEmail;
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      { firstName: this.firstName, url: this.url, subject }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html, { wordwrap: 130 }),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+}
+
+module.exports = Email;
